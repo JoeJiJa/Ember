331fa34a2c428d6d7ad42d1bc8670fc639a53024
JoeJiJa <JoeJiJa@users.noreply.github.com>
2026-08-16 20:50:29 +0530

    Fix all Gradle script and plugin configuration errors across all modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
@@ -23,9 +24,6 @@ subprojects {
     }
 
     configure<com.mikepenz.aboutlibraries.plugin.AboutLibrariesExtension> {
-        export {
-            excludeFields.addAll("generated")
-        }
     }
 }
 
