62e97ebbc1b9a0491ebb6dd4ca96310de292c9cb
JoeJiJa <JoeJiJa@users.noreply.github.com>
2026-08-19 20:51:06 +0530

    Add missing Compose BOM dependency platform(libs.androidx.compose.bom) to feature:player build.gradle.kts v2.4.5

--- feature/player/build.gradle.kts
+++ feature/player/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewModel.ktx)
     implementation(libs.google.android.material)
     implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
