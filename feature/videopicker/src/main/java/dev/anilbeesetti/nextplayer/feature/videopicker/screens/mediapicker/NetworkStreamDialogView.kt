06bcdad30ae5bfc24a47afc660a142e0fbffe762
JoeJiJa <JoeJiJa@users.noreply.github.com>
2026-08-19 21:18:14 +0530

    Add ExperimentalMaterial3Api opt-in to NetworkStreamDialogView v2.5.1

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/mediapicker/NetworkStreamDialogView.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/mediapicker/NetworkStreamDialogView.kt
@@ -18,8 +18,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.material3.ExperimentalMaterial3Api
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NetworkStreamDialogView(
     show: Boolean,
